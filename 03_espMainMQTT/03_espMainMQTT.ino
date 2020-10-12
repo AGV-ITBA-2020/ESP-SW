7ad13235c71b5d8ccfb635f0da038151d8be0312
Javier Kipen <javier1kipen@gmail.com>
2020-10-12 20:03:36 -0300

    Se probó su funcionalidad con Serial
    Al debuggearlo se mostró que ya funciona correctamente. Falta adaptar la interfaz de python y la de la CIAA

--- 03_espMainMQTT/03_espMainMQTT.ino
+++ 03_espMainMQTT/03_espMainMQTT.ino
@@ -5,9 +5,9 @@
 #define QUEUE_MAX_SIZE 10
 #define AUX_BUF_SIZE 1024
 #define HEADER_MAX_SIZE 30
-#define HB_TIME 250 //Delay de milisegundos entre señales de Heartbeat
+#define HB_TIME 3000 //Delay de milisegundos entre señales de Heartbeat
 
-#define TERMINATOR '\t'
+#define TERMINATOR 0 // \n para el debug con arduino, 0 para la ciaa
 
 
 char auxRecievedArrayMQTT[AUX_BUF_SIZE],fromCIAABuffer[AUX_BUF_SIZE],toCIAABuffer[AUX_BUF_SIZE]; //Arrays auxiliares
@@ -37,38 +37,41 @@ void HBLogic(void);
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
+  digitalWrite(BUILTIN_LED, HIGH); //Usamos el LED para indicar que se conectó a internet
   Serial.setTimeout(60000);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  handshake();
+  client.setServer(mqtt_server, 1883); //Conección al mqtt server
+  client.setCallback(callback); 
+  handshake();  //Handshake en el que la CIAA le dice el nro de AGV que es
   lastMillis = millis();
 }
 
 
-void loop() {
-  if (!client.connected()) {
+void loop() { //Loop principal 
+  if (!client.connected()) { //Si se desconectó, se conecta de vuelta
+    digitalWrite(BUILTIN_LED, HIGH);
     reconnect();
   }
-  client.loop();
-  sendToCIAAA();
-  recieveFromCIAA();
-  HBLogic(); 
+  client.loop();  //Checkea nuevos msgs recibidos
+  sendToCIAAA();  //Si tiene mensajes para la CIAA, llena el buffer de serial tx con el mensaje (no bloqueante)
+  recieveFromCIAA(); //Lee los bytes en la cola del serial Rx (No bloqueante)
+  HBLogic();      //Si pasó tanto tiempo, publica el heartbeat
 }
 
 /********************Funciones de conección**************************/
-void setup_wifi() {
+void setup_wifi() { //Conexión al Wifi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
     delay(500);
+  digitalWrite(BUILTIN_LED, LOW);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length) { //Al recibir un msj del topic del AGV
   for (int i = 0; i < length; i++) 
     auxRecievedArrayMQTT[i]=payload[i];
   auxRecievedArrayMQTT[length]=0;
-  toCIAAQueue.enqueue(String(auxRecievedArrayMQTT));
+  toCIAAQueue.enqueue(String(auxRecievedArrayMQTT)); //Encola el mensaje recibido
 }
 
 void reconnect() {
@@ -91,6 +94,7 @@ void handshake(void)
   size_t len=Serial.readBytesUntil(TERMINATOR, headerArray, HEADER_MAX_SIZE); // 00 para la ciaa, \t para debuggear con el serial de arduino
   headerArray[len]=0;
   agvHeader = String((char *)headerArray); // Garda en el string global el header que le mandó la CIAA
+  Serial.println("Llegué al header: " + agvHeader);
   String clientId = "ESP8266Client-"; // Create a random client ID
   clientId += String(random(0xffff), HEX);
   client.connect(clientId.c_str());
@@ -108,6 +112,7 @@ void recieveFromCIAA(void)
     fromCIAABuffer[fromCIAABytesCount++]=c; //Guarda el caracter recibido en un buffer
     if (c == TERMINATOR) //En el caso que termina un string
     {
+      //fromCIAABuffer[fromCIAABytesCount-1]=0; //Terminador de string para ver el string recibido. Solo es necesario cuando se debuggea con el serial
       auxStr = String((char *)fromCIAABuffer);
       if(auxStr == "Reset"){ //Si es un reset, vuelve a la rutina del principio
         while(Serial.available())
@@ -124,28 +129,28 @@ void recieveFromCIAA(void)
 }
 void sendToCIAAA(void)
 {
-  if(sendingToCIAA)
+  if(sendingToCIAA) //Si estaba mandando un mensaje a la CIAA
   {
-    int freeBuf = Serial.availableForWrite();
-    int bytesLeft = currMsgToCIAA.length() - toCIAABytesCount;
+    int freeBuf = Serial.availableForWrite(); //Espacio libre en la fifo tx de serial
+    int bytesLeft = currMsgToCIAA.length() - toCIAABytesCount; //Bytes que todavía tiene que mandar de este mensaje
     if(bytesLeft > 0)
     {
-      int bytesToSend = (bytesLeft >= freeBuf) ? freeBuf : bytesLeft;
-      Serial.write(currMsgToCIAA.substring(toCIAABytesCount).c_str(),bytesToSend);
+      int bytesToSend = (bytesLeft >= freeBuf) ? freeBuf : bytesLeft; //Si tiene que mandar más de lo que puede poner en el buffer, lo limita para que no sea bloqueante
+      Serial.write(currMsgToCIAA.substring(toCIAABytesCount).c_str(),bytesToSend); //Llena el FIFO
       toCIAABytesCount+=bytesToSend;
     }
-    else if(bytesLeft == 0)
+    else if(bytesLeft == 0) //Si terminó de escribir todo el mensaje
     {
-      while(!Serial.availableForWrite());
-      Serial.write(TERMINATOR);
+      while(!Serial.availableForWrite()); //Cuando haya espacio en la FIFO
+      Serial.write(TERMINATOR); //Pone el terminador
       sendingToCIAA=0;
     }
     
       
   }
-  else if(!toCIAAQueue.isEmpty())
+  else if(!toCIAAQueue.isEmpty()) //Si hay mensajes para mandar
   {
-    currMsgToCIAA=toCIAAQueue.dequeue();
+    currMsgToCIAA=toCIAAQueue.dequeue(); //Pullea el mensaje más viejo y resetea todo para enviarlo
     toCIAABytesCount=0;
     sendingToCIAA=true;
   }
